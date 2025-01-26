909a8458bae5c7cf4e75c58af7f856c1a1a2abe8
Salman Farid Rahman <salman.f.rahman@g.bracu.ac.bd>
2025-01-26 17:14:41 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/drag_pdf/MainActivity.kt
@@ -0,0 +1,5 @@
+package es.victorcarreras.drag_pdf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import 'drag_pdf_app.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const DragPdfApp());
+}
